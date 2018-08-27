63577f19a34df3cc5db689997981d906f3f29744
rafahmnjd <40000159+rafahmnjd@users.noreply.github.com>
2018-08-28 02:23:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/toshiba/shelter/ABOUT.java
@@ -0,0 +1,13 @@
+package com.example.toshiba.shelter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ABOUT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
