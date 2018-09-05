bd191f961f36acb220d74ea8b9bce911438946d0
rafahmnjd <rafahmnjd@gmail.com>
2018-09-05 13:37:33 +0300

    add SupportsActivity

---
+++ app/src/main/java/com/example/toshiba/shelter/OurSupporters.java
@@ -0,0 +1,13 @@
+package com.example.toshiba.shelter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OurSupporters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_our_supporters);
+    }
+}
